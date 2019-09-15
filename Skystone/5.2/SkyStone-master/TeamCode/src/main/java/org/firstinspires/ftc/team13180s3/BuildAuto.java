c318f1766b9d4d57753f45973c15c3d71dfe77a4
Rohan <40905346+Rohan1215@users.noreply.github.com>
2019-09-15 13:21:20 -0700

    Build Auto+TestServo

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildAuto.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildAuto.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name="BuildAuto", group="autonomusGroup1")
 
 public class BuildAuto extends LinearOpMode {
+    private RoboNavigator robonav;
 
 
     @Override
--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/TestServo.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/TestServo.java
@@ -1,19 +1,22 @@
 package org.firstinspires.ftc.team13180s3;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.CRServo;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+@TeleOp(name="TestServo", group="manualmode")
 public class TestServo extends LinearOpMode{
-    private LinearOpMode opMode;
+    public LinearOpMode opMode;
     private CRServo TestServo;
+
     public void runOpMode()  {
-        while opModeIsActive(){
+        TestServo=opMode.hardwareMap.get(CRServo.class,"Ser");
+        while (opModeIsActive()){
+
             double rx=gamepad1.left_stick_x;
             if(rx>0) {
                 TestServo.setPower(0.6);
