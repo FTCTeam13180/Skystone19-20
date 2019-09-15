5c8180d82878347762c69637081aaf7a023f98d7
Rohan <40905346+Rohan1215@users.noreply.github.com>
2019-09-15 11:30:48 -0700

    Putting autonomous code in skystone repos

---
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/LandAuto.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.team13180s3;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Autonomous
+public class LandAuto extends LinearOpMode {
+    private RoboNavigator roboNavigator;
+
+    @Override
+    public void runOpMode () {
+
+
+
+
+        }
+    }
+
+
+
+
