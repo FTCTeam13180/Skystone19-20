51e29ab457cb8ab47ca177e6753548e37a85ad68
Shivam Adeshara <sadeshara26@gmail.com>
2019-09-15 11:13:21 -0700

    9/15/19

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildAuto.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.team13180s3;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+
+
+
+@Autonomous(name="BuildAuto", group="autonomusGroup1")
+
+public class BuildAuto extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode () {
+
+
+
+        }
+    }
+
+
