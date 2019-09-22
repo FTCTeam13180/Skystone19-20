bb802f4ce51f587d3aecca31ec188ab98dccd832
Shivam Adeshara <sadeshara26@gmail.com>
2019-09-22 13:07:08 -0700

    Fix the missing AndroidManifest.xml file

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildAuto.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildAuto.java
@@ -4,21 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 
-
-
-
 @Autonomous(name="BuildAuto", group="autonomusGroup1")
-
 public class BuildAuto extends LinearOpMode {
-    private RoboNavigator robonav;
-
+    private RoboNavigator robotNavigator;
 
     @Override
     public void runOpMode () {
+        robotNavigator = new RoboNavigator(this) ;
+        robotNavigator.init() ;
+        double NAVIGATER_POWER = 0.5; // check
 
+        //waitforstart() ;
 
-
+        try {
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT, 0.5, 36, 10000);
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATER_POWER, 15, 10000);
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATER_POWER, 40, 10000);
+        } catch(Exception e) {
         }
     }
-
-
+}
