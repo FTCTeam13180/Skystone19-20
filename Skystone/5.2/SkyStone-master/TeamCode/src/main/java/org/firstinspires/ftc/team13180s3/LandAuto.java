7db6bffeacdebbcdcb9bcbd3bfebd0976efc6200
Shivam Adeshara <sadeshara26@gmail.com>
2019-10-11 20:01:11 -0700

    update color sensor

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/LandAuto.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/LandAuto.java
@@ -1,22 +1,25 @@
 package org.firstinspires.ftc.team13180s3;
-
+//osmehting
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
-@Autonomous
+
+@Autonomous(name="LandAuto", group="autonomusGroup1")
 public class LandAuto extends LinearOpMode {
-    private RoboNavigator roboNavigator;
+    private RoboNavigator robotNavigator;
 
     @Override
-    public void runOpMode () {
-
-
-
+    public void runOpMode() {
+        robotNavigator = new RoboNavigator(this);
+        robotNavigator.init();
+        double NAVIGATOR_POWER = 0.5; // check
+        waitForStart();
+        while (opModeIsActive()) {
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 31.23*2.54, 10000);
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT, NAVIGATOR_POWER, 31.25*2.54, 10000);
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 40*2.54, 10000);
 
         }
     }
-
-
-
-
+}
