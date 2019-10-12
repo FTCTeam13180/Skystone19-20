772256cad654e1cc7defbf43fb62b085d8ca7da7
Rohan <40905346+Rohan1215@users.noreply.github.com>
2019-10-11 21:27:10 -0700

    heh

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/RoboNavigator.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/RoboNavigator.java
@@ -304,7 +304,7 @@ public class RoboNavigator {
      *
      */
     private static  final double ROBO_DIAMETER_CM = 61;
-    private static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    private static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder
     private static final double     DRIVE_GEAR_REDUCTION    = 0.776 ;     // This is < 1.0 if geared up
     private static final double     WHEEL_DIAMETER_CM   = 10.16 ;     // For figuring circumference
     private static final double     COUNTS_PER_CM         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
