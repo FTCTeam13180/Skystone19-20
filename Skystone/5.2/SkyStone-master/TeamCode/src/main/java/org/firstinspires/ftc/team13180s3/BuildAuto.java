3e0625417fbf2b3becf2a32462bf0f517cdfbda4
Yash Pradhan <yashpradhan181@gmail.com>
2019-10-13 13:33:56 -0700

    auto nav code-doesn't work

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildAuto.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildAuto.java
@@ -17,9 +17,32 @@ public class BuildAuto extends LinearOpMode {
         while (opModeIsActive()) {
             robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 31.25*2.54, 10000);
             robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT, NAVIGATOR_POWER, 31.25*2.54, 10000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 40*2.54, 10000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 40*2.54, 10000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 40*2.54, 10000);
+            //pulls the foundation to the triangle build tape zone
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 20*2.54, 10000);
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 12*2.54, 10000);
+            //shifts left to align with bridge, haven't crossed yet
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 64*2.54, 10000);
+            //goes under bridge
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 24*2.54, 10000);
+
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.TURN_LEFT, NAVIGATOR_POWER, 90, 10000);
+            //HERE IS WHERE YOU PUT IN THE BLOCK SCANNING CODE
+            //HERE IS WHERE YOU PUT IN THE BLOCK SCANNING CODE
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT, NAVIGATOR_POWER, 24*2.54, 10000);
+            //scanning the blocks
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 36*2.54, 10000);
+            //go to the other side of the block line
+            //now we assume that the robot has detected a skystone
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 12*2.54, 10000);
+            //GRipper takes block in
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 96*2.54, 10000);
+            //goes back to building zone
+            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.BACKWARD, NAVIGATOR_POWER, 24*2.54, 10000);
+            //MOVES NEXT TO the foundation
+            //code to deposit the skystone in foundation
+
+
+
 
         }
     }
