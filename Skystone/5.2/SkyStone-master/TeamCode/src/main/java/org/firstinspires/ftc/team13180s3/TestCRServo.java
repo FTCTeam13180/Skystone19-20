ae1131fb569d2635e7a9cd2045d96b2e39e74fdb
Yash Pradhan <yashpradhan181@gmail.com>
2019-10-19 15:59:56 -0700

    fixed configs and test servo classes

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/TestCRServo.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/TestCRServo.java
@@ -1,31 +1,32 @@
 package org.firstinspires.ftc.team13180s3;
-//Rohan Gulati -10/5/2019
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
-@TeleOp(name="TestServo", group="manualmode")
-public class TestServo extends LinearOpMode{
+@TeleOp(name="TestCRServo", group="manualmode")
+public class TestCRServo extends LinearOpMode{
     public LinearOpMode opMode = this;
-    private Servo servo_1;
+    private CRServo servo_1;
 
     public void runOpMode()  {
         waitForStart();
-        servo_1=opMode.hardwareMap.get(Servo.class,"Servo");
+        servo_1=opMode.hardwareMap.get(CRServo.class,"CRServo");
         while (opModeIsActive()){
 
             double rx=gamepad1.left_stick_x;
             if(rx>0) {
-                servo_1.setPosition(1.0);
+                servo_1.setPower(0.8);
             }
             else if(rx<0) {
-                servo_1.setPosition(-1.0);
+                servo_1.setPower(-0.8);
             }
             else{
-                servo_1.setPosition(0);
+                servo_1.setPower(0);
             }
 
 
         }
     }
+
 }
