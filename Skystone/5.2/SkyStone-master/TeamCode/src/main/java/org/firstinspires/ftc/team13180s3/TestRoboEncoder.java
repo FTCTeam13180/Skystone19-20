3c6761d513326e8d8a71eb061d003916def120e0
Shivam Adeshara <sadeshara26@gmail.com>
2019-11-16 13:41:29 -0800

    Update TestRoboEncoder.java

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/TestRoboEncoder.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/TestRoboEncoder.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.team13180s3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "TestRoboEncoder", group = "manualmode")
 public class TestRoboEncoder extends LinearOpMode {
@@ -12,17 +13,20 @@ public class TestRoboEncoder extends LinearOpMode {
 
         dcMotor = hardwareMap.get(DcMotor.class, "dcMotor");
         telemetry.addData("TestRoboEncoder", "Initialized");
+        dcMotor.setDirection(DcMotor.Direction.FORWARD);
+        dcMotor.setPower(0);
         dcMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         dcMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart();
 
         while(opModeIsActive()){
             if(gamepad1.dpad_up) {
-                dcMotor.setTargetPosition(1440);
+                dcMotor.setTargetPosition(dcMotor.getCurrentPosition() + 1440);
                 dcMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 dcMotor.setPower(10);
                 while(dcMotor.isBusy()){
-                    telemetry.addData("Encoder ValueS = ", "%d", dcMotor.getCurrentPosition());
+                    telemetry.addData("IsBusy() cur = ", "%7d", dcMotor.getCurrentPosition());
                     telemetry.update();
                     idle();
                 }
@@ -31,7 +35,7 @@ public class TestRoboEncoder extends LinearOpMode {
 
             }
             if (gamepad1.a) {
-                telemetry.addData("Encoder ValueS = ", "%d", dcMotor.getCurrentPosition());
+                telemetry.addData("Encoder ValueS = ", "%7d", dcMotor.getCurrentPosition());
                 telemetry.update();
             }
         }
