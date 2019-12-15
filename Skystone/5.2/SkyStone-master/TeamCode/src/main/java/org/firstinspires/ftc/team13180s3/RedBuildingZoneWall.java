8b4cc48e67a24e30e851ae234b804773fa652b7a
Pratyush Seth <pratyushseth@gmail.com>
2019-12-15 09:33:31 -0800

    Migrated all 4 buildingzone autonomous modes to BuildingZoneFullAuto

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/RedBuildingZoneWall.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/RedBuildingZoneWall.java
@@ -6,80 +6,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name="RedBuildingZoneWall", group="autonomusGroup1")
 public class RedBuildingZoneWall extends LinearOpMode {
-    private RoboNavigator robotNavigator;
-    private Hook hook;
-    private Grabber grab;
-    private Elevator elevator;
+    private BuildingZoneFullAuto fullAuto;
 
     @Override
     public void runOpMode() {
-        robotNavigator = new RoboNavigator(this);
-        robotNavigator.init();
-        hook = new Hook(this);
-        hook.init();
-        grab = new Grabber(this);
-        grab.init();
-        elevator = new Elevator(this);
-        elevator.init();
-
-        double NAVIGATOR_POWER = 0.5; // check
-
-        hook.detach();
+        fullAuto = new BuildingZoneFullAuto(
+                this,
+                BuildingZoneFullAuto.Alliance.RED,
+                BuildingZoneFullAuto.Parking.WALL);
 
         waitForStart();
 
         while (opModeIsActive()) {
-
-            //goes up to the foundation to move it into the building zone
-            //winchUp.encoderDrive(NAVIGATOR_POWER,10*2.54,1000);
-
-            //winch down
-            hook.halfattach(0.5);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 29* 2.54, 5000);
-            sleep(1000);
-
-            //HOOK comes down heref
-
-            hook.attach();
-
-            sleep(1500);
-
-            //pulls the foundation to the triangle build ZONE
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.TURN_RIGHT, NAVIGATOR_POWER, 210, 3000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD,NAVIGATOR_POWER,40*2.54,2000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT,NAVIGATOR_POWER,36*2.54,2000);
-            sleep(1000);
-            hook.detach();
-            sleep(1000);
-
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.BACKWARD,NAVIGATOR_POWER,5*2.54,2000);
-            hook.attach();
-            elevator.goOutByRotations(0.8,4);
-            hook.detach();
-            elevator.upDownEncoderDrive(NAVIGATOR_POWER,5*2.54,1000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.BACKWARD,NAVIGATOR_POWER,36*2.54,2000);
-            //robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD,NAVIGATOR_POWER,20*2.54,2000);
-
-            // elevator.goOutTime(0.6, 1000);
-            //winchUp.goDownTime(0.6, 2000);
-            //robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 24, 10000);
-            //elevator.goOutTime(0.6, 2000);
-            /*robotNavigator.encoderDrive(RoboNav0igator.DIRECTION.BACKWARD, NAVIGATOR_POWER, 44*2.54, 10000);
-            //going around the foundation to the other side of it
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT, NAVIGATOR_POWER, 35*2.54, 10000);
-
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 12*2.54, 10000);
-            //pushes the foundation completely into the building zone
-            //8 in to the wall of                                                      the build zone (Driver station side)
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 35*2.54, 10000);
-
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 5*2.54, 10000);
-
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 20*2.54, 10000);
-            //ROBOT IS PARKED UNDER THE ALLIANCE SKYBRIDGE*/
-
+            fullAuto.run();
             break;
-
         }
     }
 }
