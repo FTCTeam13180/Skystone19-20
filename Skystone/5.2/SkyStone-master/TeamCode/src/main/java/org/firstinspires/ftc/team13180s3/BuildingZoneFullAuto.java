5547676eba74a52229412eb86f20fcdf6f293854
Yash Pradhan <yashpradhan181@gmail.com>
2019-12-15 12:59:02 -0800

    Update BuildingZoneFullAuto.java
    wrote program for building zone wall side auto

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildingZoneFullAuto.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/BuildingZoneFullAuto.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.team13180s3;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 
 public class BuildingZoneFullAuto {
@@ -46,12 +44,12 @@ public class BuildingZoneFullAuto {
         elevator = new Elevator(opMode);
         elevator.init();
 
-
+        opMode.sleep(20000);
         // Set hook in attach position
         hook.halfattach(0.5);
 
         // Go to Foundation and grab it
-        robotNavigator.moveForward(29, 10000);
+        robotNavigator.moveBackward(29, 10000);
         opMode.sleep(200);
         hook.attach();
         opMode.sleep(200);
@@ -60,22 +58,20 @@ public class BuildingZoneFullAuto {
         /*
          * Move the foundation to the triangle build ZONE
          */
-        // TURN horizontal
+        // Move foundation away from opposing alliance foundation
+        robotNavigator.moveForward(12, 10000);
+        //pull foundation towards the bridges, away from north wall
+        // then turning the foundation
         double turn_degrees = 210;
         if (alliance == Alliance.BLUE)
+            robotNavigator.shiftLeft(12, 10000);
             robotNavigator.turnLeft(turn_degrees, 10000);
         else
+            robotNavigator.shiftRight(12, 10000);
             robotNavigator.turnRight(turn_degrees, 10000);
 
-        // Allign with field perimeter
-        robotNavigator.moveForward(40,10000);
-
-        // Drag to the corner
-        double shift_inches = 36;
-        if (alliance == Alliance.BLUE)
-            robotNavigator.shiftLeft(shift_inches,10000);
-        else
-            robotNavigator.shiftRight(shift_inches,10000);
+        // Pushing foundation into depot
+        robotNavigator.moveBackward(24,10000);
 
         /*
          * Detach from the foundation
@@ -83,7 +79,7 @@ public class BuildingZoneFullAuto {
         opMode.sleep(200);
         hook.detach();
         opMode.sleep(200);
-        robotNavigator.moveBackward(5,10000);
+        robotNavigator.moveForward(6,10000);
 
         /*
          * Lower the elevator to fit under bridge, move the hook out of way first.
@@ -96,13 +92,13 @@ public class BuildingZoneFullAuto {
         /*
          * Move the robot under the bridge
          */
-        if (parking == Parking.BRIDGE) {
+        if (parking == Parking.WALL) {
             if (alliance == Alliance.BLUE)
-                robotNavigator.shiftRight(24, 10000);
+                robotNavigator.shiftRight(18, 10000);
             else
-                robotNavigator.shiftLeft(24, 10000);
+                robotNavigator.shiftLeft(18, 10000);
         }
-        robotNavigator.moveBackward(36,10000);
+        robotNavigator.moveForward(36,10000);
 
         // ROBOT IS PARKED UNDER THE ALLIANCE SKYBRIDGE*/
     }
