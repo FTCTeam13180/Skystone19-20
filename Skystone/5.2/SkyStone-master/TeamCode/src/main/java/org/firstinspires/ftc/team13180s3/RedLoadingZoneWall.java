c0c43be0ddd5b00bd3afd1c73961344357669be1
Rohan <40905346+Rohan1215@users.noreply.github.com>
2020-01-04 18:55:56 -0800

    update loading zone

--- Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/RedLoadingZoneWall.java
+++ Skystone/5.2/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/team13180s3/RedLoadingZoneWall.java
@@ -6,89 +6,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name="RedLoadingZoneWall", group="autonomusGroup1")
 public class RedLoadingZoneWall extends LinearOpMode {
-    private RoboNavigator robotNavigator;
-    private Grabber grab;
-    private Hook hook;
-    private Elevator elevator;
-
+    private LoadingZoneFullAuto fullAuto;
     @Override
     public void runOpMode() {
-        robotNavigator = new RoboNavigator(this);
-        robotNavigator.init();
-
-        //TODO
-        // stoneSenor = new StoneColorSensor(this);
-        //stoneSenor.init();
-
-        //intake = new Intake(this);
-        //intake.init();
-
-        grab = new Grabber(this);
-        grab.init();
-        hook= new Hook(this);
-        hook.init();
-        elevator =new Elevator(this);
-        elevator.init();
-        double NAVIGATOR_POWER = 0.5; // check
-        hook.detach();
+        fullAuto= new LoadingZoneFullAuto(this, LoadingZoneFullAuto.Alliance.RED, LoadingZoneFullAuto.Parking.WALL);
         waitForStart();
-        while (opModeIsActive()) {
-            //This is assuming that we are next to the blue depot
-            telemetry.addLine("Front of Robot Should be forward");
-            telemetry.update();
-            //winch up(TODO)
-            // winch.up()
-            // go forward 25 inches
-
-            elevator.upDownEncoderDrive(NAVIGATOR_POWER,-2.5*2.54,200);
-            grab.release();
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 25*2.54, 4000);
-            //winch out 5 inch
-            //grab.grabIn();
-            //winch down
-            elevator.goOutByRotations(0.8,4);
-            sleep(1000);
-            elevator.upDownEncoderDrive(NAVIGATOR_POWER,6*2.54,2200);
-            sleep(500);
-            grab.grabIn();
-            sleep(500);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.BACKWARD,NAVIGATOR_POWER,15*2.54,10000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.TURN_RIGHT,NAVIGATOR_POWER,100,5000);
-//            upWinch.encoderDrive(NAVIGATOR_POWER,5*2.54,1200);
-
-//            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT, NAVIGATOR_POWER, 4*2.54, 1000);
-//            grab.grabIn();
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER,44*2.54,10000);
-            grab.release();
-            //grab.release();
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.BACKWARD,NAVIGATOR_POWER,68*2.54,10000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT,NAVIGATOR_POWER,5*2.54,10000);
-            sleep(5000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD,NAVIGATOR_POWER,54*2.54,10000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT,NAVIGATOR_POWER,7*2.54,10000);
-
-            /*robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 42*2.54, 10000);
-            robotNavigator.encoderDrive(RoboNavigator.DIRECTION.BACKWARD, NAVIGATOR_POWER, 24*2.54, 10000);
-            while(stoneSenor.isBlackColor() == false){
-                stoneSenor.getColorSensor();
-                if (stoneSenor.isYellowColor() == true){
-                    robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER,6*2.54, 10000);
-                }
-                else if(stoneSenor.isBlackColor()){
-                    robotNavigator.encoderDrive(RoboNavigator.DIRECTION.BACKWARD, NAVIGATOR_POWER, 6*2.54, 10000);
-                    robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_LEFT, NAVIGATOR_POWER, 6*2.54, 10000);
-                    robotNavigator.encoderDrive(RoboNavigator.DIRECTION.FORWARD, NAVIGATOR_POWER, 6*2.54, 10000);
-                    intake.startIntake(0.6);
-                    robotNavigator.encoderDrive(RoboNavigator.DIRECTION.SHIFT_RIGHT, NAVIGATOR_POWER, 12*2.54, 10000);
-                    break;
-
-
-
-                }
-
-            }
-*/
+        while(opModeIsActive()){
+            fullAuto.run();
             break;
         }
+
     }
 }
